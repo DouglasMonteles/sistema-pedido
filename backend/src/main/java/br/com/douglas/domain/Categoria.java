60a5b6cadfb57e7095bf420becb0298f4e708e9d
DouglasMonteles <silvadouglas328@gmail.com>
2020-07-15 18:53:38 -0300

    Banco de dados H2 e criacao automatica da base de dados

--- backend/src/main/java/br/com/douglas/domain/Categoria.java
+++ backend/src/main/java/br/com/douglas/domain/Categoria.java
@@ -2,10 +2,20 @@ package br.com.douglas.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity(name = "categoria")
 public class Categoria implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(generator = "increment")
+	@GenericGenerator(name = "increment", strategy = "increment")
 	private Integer id;
 	private String nome;
 	
