6d25c24f24766d5187e7c3ff60b232849bc51ea7
DouglasMonteles <silvadouglas328@gmail.com>
2020-07-15 20:21:31 -0300

    Produto e associacao muitos para muitos

--- backend/src/main/java/br/com/douglas/BackendApplication.java
+++ backend/src/main/java/br/com/douglas/BackendApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.douglas.domain.Categoria;
+import br.com.douglas.domain.Produto;
 import br.com.douglas.repository.CategoriaRepository;
+import br.com.douglas.repository.ProdutoRepository;
 
 @SpringBootApplication
 public class BackendApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
@@ -26,7 +31,19 @@ public class BackendApplication implements CommandLineRunner {
 		Categoria c1 = new Categoria(null, "Informática");
 		Categoria c2 = new Categoria(null, "Cama, mesa e banho");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		c1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		c2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(c1));
+		p2.getCategorias().addAll(Arrays.asList(c1, c2));
+		p3.getCategorias().addAll(Arrays.asList(c1));
+		
 		categoriaRepository.saveAll(Arrays.asList(c1, c2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
 	}
 
--- backend/src/main/java/br/com/douglas/domain/Categoria.java
+++ backend/src/main/java/br/com/douglas/domain/Categoria.java
@@ -1,10 +1,13 @@
 package br.com.douglas.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 import org.hibernate.annotations.GenericGenerator;
 
@@ -18,6 +21,9 @@ public class Categoria implements Serializable {
 	@GenericGenerator(name = "increment", strategy = "increment")
 	private Integer id;
 	private String nome;
+	
+	@ManyToMany(mappedBy = "categorias")
+	private List<Produto> produtos = new ArrayList<>();
 	
 	public Categoria() {}
 	
@@ -41,6 +47,14 @@ public class Categoria implements Serializable {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+	
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+	
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
 	}
 
 	@Override
