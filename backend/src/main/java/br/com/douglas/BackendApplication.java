45cc0523b2e83f73dbe3a958e0aa5ed6d3253f97
DouglasMonteles <silvadouglas328@gmail.com>
2020-07-15 19:18:23 -0300

    Criando operacao de instanciacao

--- backend/src/main/java/br/com/douglas/BackendApplication.java
+++ backend/src/main/java/br/com/douglas/BackendApplication.java
@@ -1,13 +1,33 @@
 package br.com.douglas;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class BackendApplication {
+import br.com.douglas.domain.Categoria;
+import br.com.douglas.repository.CategoriaRepository;
 
+@SpringBootApplication
+public class BackendApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria c1 = new Categoria(null, "Informática");
+		Categoria c2 = new Categoria(null, "Cama, mesa e banho");
+		
+		categoriaRepository.saveAll(Arrays.asList(c1, c2));
+		
 	}
 
 }
